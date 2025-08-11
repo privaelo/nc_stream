e02dc3803e500a2edff5c7578f625b35b5b54368
privaelo <privaeloking@gmail.com>
2025-08-10 23:16:13 -0400

    Updated cli, requirements and setup.py

---
+++ nc_stream/cli.py
@@ -0,0 +1,15 @@
+import argparse
+from . import stream_netcdf
+
+def main():
+    parser = argparse.ArgumentParser(description="Stream Sentinel-5P NetCDF from S3")
+    parser.add_argument("--bucket", required=True)
+    parser.add_argument("--key", required=True)
+    parser.add_argument("--group", default="/PRODUCT")
+    args = parser.parse_args()
+
+    ds = stream_netcdf(args.bucket, args.key, args.group)
+    print(ds)
+
+if __name__ == "__main__":
+    main()
